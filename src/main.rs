54067884947f2fb5d08b4e24c4f96b1c72d57d28
rgimad <33692565+rgimad@users.noreply.github.com>
2025-04-25 14:14:35 +0300

    Basic cli args processing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,36 @@
-use macroquad::prelude::*;
+use std::env;
+use std::path::Path;
+// use macroquad::prelude::*;
 
-#[macroquad::main("KolibriOS skin viewer")]
-async fn main() {
-    loop {
-        clear_background(RED);
+const SKIN_MAGIC: i32  = 0x4E494B53; // 'SKIN'
+const KPACK_MAGIC: i32 = 0x4B43504B; // 'KPACK'
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
 
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+// #[macroquad::main("KolibriOS skin viewer")] async
+fn main() {
+    let args: Vec<String> = env::args().collect();
 
-        next_frame().await
+    if args.len() < 2 {
+        println!("Usage: {} <filename>", Path::new(&args[0]).file_name().unwrap().to_str().unwrap());
+        std::process::exit(1);
     }
+
+    let input_file_path = Path::new(&args[1]);
+
+    if !input_file_path.exists() || !input_file_path.is_file() {
+        println!("File '{}' does not exist", input_file_path.display());
+    }
+
+
+    // loop {
+    //     clear_background(RED);
+
+    //     draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+    //     draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+    //     draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+
+    //     draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+
+    //     next_frame().await
+    // }
 }
